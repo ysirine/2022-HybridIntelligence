70dee17d1b8c3628c5f7eb383dae1c094511e305
Sirine Younsi <103052693+ysirine@users.noreply.github.com>
2022-05-24 20:00:53 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+import aicrowd_helper
+import train_submission_code
+import testKairos
+
+import os
+EVALUATION_RUNNING_ON = os.getenv('EVALUATION_RUNNING_ON', None)
+EVALUATION_STAGE='testing'
+EXITED_SIGNAL_PATH = os.getenv('EXITED_SIGNAL_PATH', 'shared/exited')
+
+if EVALUATION_STAGE in ['all', 'testing']:
+    if EVALUATION_RUNNING_ON in ['local']:
+        try:
+            os.remove(EXITED_SIGNAL_PATH)
+        except FileNotFoundError:
+            pass
+    aicrowd_helper.inference_start()
+    try:
+        testKairos.main()
+        aicrowd_helper.inference_end()
+    except Exception as e:
+        aicrowd_helper.inference_error()
+        print(e)
+    if EVALUATION_RUNNING_ON in ['local']:
+        from pathlib import Path
+        Path(EXITED_SIGNAL_PATH).touch()
